0dfb48906ac18ac6fb79c45e4095c8af1293f4ba
zhao-xi <zhaoxi0418@outlook.com>
2020-08-02 16:55:49 +0900

    20200801

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] cubes;
+    public Transform[] points;
+    public float interval = 1f;
+    private float timer;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 2f)
+        {
+            GameObject cube = Instantiate(
+                cubes[Random.Range(0,cubes.Length)],
+                points[Random.Range(0,points.Length)]
+                );
+            cube.AddComponent<DestroyCube>();
+            cube.transform.localPosition = Vector3.zero;
+            timer -= interval;
+        }
+        timer += Time.deltaTime;
+    }
+}
