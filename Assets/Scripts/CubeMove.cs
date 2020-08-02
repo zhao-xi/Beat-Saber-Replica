83a805af07eefad884f1a8cc2b64dfe7f59cb6ab
zhao-xi <zhaoxi0418@outlook.com>
2020-08-02 21:35:59 +0900

    when spawn cubes, make its size gradually grow from 0 to 1

--- Assets/Scripts/CubeMove.cs
+++ Assets/Scripts/CubeMove.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CubeMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float scale = 0f;
 
     // Update is called once per frame
     void Update()
     {
+        if(scale < 1f)
+        {
+            scale += Time.deltaTime * 4;
+            if (scale > 1f) scale = 1f;
+            transform.localScale = new Vector3(scale, scale, scale);
+        }
+        
         transform.position += Time.deltaTime * transform.forward * 4;
     }
 }
